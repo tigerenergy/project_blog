41b445f7f45f50a682ab6f13f9022a46189f1cc7
kiwoon kim <kisoon0947@gmail.com>
2021-10-06 20:59:23 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
+import './App.css'
+import { Reset } from 'styled-reset'
+function App()
+{
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+      <div>개발 블로그</div>
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
