794207f6f53609c95de46908e6da87113dba4674
kiwoon kim <kisoon0947@gmail.com>
2021-10-07 01:48:19 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React, { useState } from 'react'
 import './App.css'
-import { Reset } from 'styled-reset'
+
+
 function App()
 {
+
+  let [title, titleChange] = useState(['평양 냉면 추천' , '소고기 맛집', '돼지고기 맛집',])
+  let [good, goodChange] = useState(0)
+  
+
+  const changeTitle = () =>
+  {
+    const newArray = [...title]
+    newArray[0] = '피자 치킨 맛집'
+    titleChange(newArray)
+  }
+
+
   return(
     <div className="App">
+      
       <div className="black-nav">
       <div>개발 블로그</div>
+      <button className="btn" onClick={ changeTitle }>버튼</button>
+    </div>
+    <div className="list">
+      <h3>{ title[0] } <span onClick={()=> { goodChange(good + 1) }} >👍</span> { good } </h3>
+      <p>10월 8일 발행</p>
+      <hr/>
+    </div>
+    <div className="list">
+      <h3>{ title[1] }</h3>
+      <p>10월 8일 발행</p>
+      <hr/>
+    </div>
+    <div className="list">
+      <h3>{ title[2] }</h3>
+      <p>10월 8일 발행</p>
+      <hr/>
     </div>
   </div>
   )
