892065c1483f81d8f952a67b86dacdef2bdd9d38
kiwoon kim <kisoon0947@gmail.com>
2021-10-10 20:55:42 +0900

    update

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App()
 
   let [titleSet, titleChange] = useState(['평양 냉면 추천' , '소고기 맛집', '돼지고기 맛집'])
   let [goodSet, goodChange] = useState([0,0,0])
-  let [inputSet , inputChange] = useState('')
   let [modal, modalChange] = useState(false)
+  let [inputSet , inputChange] = useState('')
   let [clickTitle, clickTitleChange] = useState(0)
 
-
+  let [inputData, inputDataChange] = useState('')
   const changeInput = (i) =>
   {
     const newArray = [...goodSet]
@@ -21,15 +21,15 @@ function App()
 
  {/* 반복문 함수를 만들어 사용하는 경우 */}
 
-  const htmlTitle = () =>
-  {
-    const Array = [];
-    for (let i = 0; i < 3; i++) 
-    {
-      Array.push(<div>안녕</div>)
-    }
-    return Array
-  }
+  // const htmlTitle = () =>
+  // {
+  //   const Array = [];
+  //   for (let i = 0; i < 3; i++) 
+  //   {
+  //     Array.push(<div>안녕</div>)
+  //   }
+  //   return Array
+  // }
 
   return(
     <div className="App">
@@ -40,19 +40,29 @@ function App()
     {/* 반복문  map을 사용할 것 */}
 
     {
-      titleSet.map((value, Point)=>
+      titleSet.map((value, i)=>
       {
         return(
-          <div className="list" key='{Point}'>
-          <h3 onClick={()=>{clickTitleChange(Point)}}>{ value } </h3>
-          <span onClick={()=> {changeInput(Point) }} >👍</span> { goodSet[Point] } 
+          <div className="list" key='{i}'>
+          <h3 onClick={()=>{clickTitleChange(i)}}>{ value } </h3>
+          <span onClick={()=> {changeInput(i) }} >👍</span> { goodSet[i] } 
           <p>10월 8일 발행</p>
-            { htmlTitle() }
           <hr/>
         </div>
         )  
       })
     }
+
+
+    <div className='inputBox'>
+      <input placeholder="입력해주세요" onChange={(e)=>{inputDataChange(e.target.value)} }></input>
+
+      <button onClick={()=>{
+      const titleNewArray = [...titleSet] 
+      titleNewArray.unshift(inputData)
+      titleChange(titleNewArray)
+      }}>저장</button>
+    </div>
 
     <div className="buttons">
     <button onClick={()=>{modalChange(!modal)}}>열고닫기</button>
